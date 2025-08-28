dd627f313051c3d48b37da1b0f89b7366217633c
Byron Arenilla <124713452+ByronArenilla@users.noreply.github.com>
2025-08-28 18:39:21 -0500

    commit before change

--- Intermidate_importing_data_in_python/08streming_tweets.py
+++ Intermidate_importing_data_in_python/08streming_tweets.py
@@ -1,5 +1,3 @@
-# Twitter data to DataFrame
-# Now you have the Twitter data in a list of dictionaries, tweets_data, where each dictionary corresponds to a single tweet. Next, you're going to extract the text and language of each tweet. The text in a tweet, t1, is stored as the value t1['text']; similarly, the language is stored in t1['lang']. Your task is to build a DataFrame in which each row is a tweet and the columns are 'text' and 'lang'.
 
 import tweepy
 # Store credentials in relevant variables
